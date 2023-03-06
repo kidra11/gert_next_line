f77f8df04474fa5a58e98a45fb3c255da0b4ee47
Nathalie Sion <nsion@c2r7p5.42perpignan.fr>
2023-03-06 16:02:09 +0100

    main

--- get_next_line.c
+++ get_next_line.c
@@ -3,33 +3,146 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nsion <nsion@student.42perpignan.fr>       +#+  +:+       +#+        */
+/*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/01 01:02:59 by nsion             #+#    #+#             */
-/*   Updated: 2023/03/01 01:29:03 by nsion            ###   ########.fr       */
+/*   Created: 2023/02/27 19:42:39 by nsion             #+#    #+#             */
+/*   Updated: 2023/03/04 19:31:57 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+int	ft_strlen(char s)
 {
-	static char	*stock;
-	char	*line;
-	char	buf[size];
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		l;
+	int		i;
+	int		k;
+
+	if (!s1)
+		return (s2);
+	else if (!s2)
+		return ((char *)s1);
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	l = 0;
+	i = 0;
+	k = 0;
+	while (s1[i])
+		str[l++] = s1[i++];
+	while (s2[k])
+		str[l++] = s2[k++];
+	str[l] = '\0';
+	return (str);
+}
+
+char	*copy_line(char *stat)
+{
+	char *line;
 	int	i;
 
-	stock = (char *) malloc(sizeof(char) * ?? + 1);
-	if (!stock)
-		return (NULL);
-	while (?? == '\n')
-	stock += read(fd, buf, size);
-	line = (char *) malloc(sizeof(char) * ?? + 1);
-	if (!line)
-		return (NULL;
-	i = ft_strlen(stock);
-	while (?i != '\n')
-		line[i++] = stock[i++];
-	buf[line] = '\0';
-	return (buf[line] || line??)
+	if...
+		...
+	...
+	line = (char *) malloc(sizeof(char) * find_end(stat) + 2);
+        if (!line)
+                return (NULL);
+        i = -1;
+        while (stat[++i] != '\n' && stat[i])
+                line[i] = stat[i];
+        line[i] = '\n';
+        line[i++] = '\0';
+	return (line);
 }
+
+int	find_end(char *stat)
+{
+	int	i;
+
+	i = 0;
+	while (stat[i])
+	{
+		if (stat[i] != '\n')
+			i++;
+		else
+			return (i);
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	ssize_t		stock;
+	static char	*next;
+	char		*stat;
+	char		*line;
+	char		*buf;
+
+	buf = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	stat = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!stat)
+		return (NULL);
+	if (next)
+		stat = ft_strjoin(next, stat);
+	while (find_end(stat) > 0 || stock > 0)
+	{		
+		stock = read(fd, buf, BUFFER_SIZE);
+		buf[stock] = '\0';
+		stat = ft_strjoin(stat, buf);
+	}
+	printf("buf = %s\n", buf);
+	free(buf);
+	line = copy_line(stat);
+	stock = ft_strlen(line);
+	next = (char *) malloc(sizeof(char) * (ft_strlen(stat) - stock) + 1);
+	if (!next)
+		return (NULL);
+	stock = -1;
+	while (stat[++stock])
+		next[stock] = stat[ft_strlen(line) + stock];
+	free(stat);
+	return (line);
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	int	fd;
+	char	*str;
+
+	fd = open("text", O_RDONLY);
+	str = get_next_line(fd);
+	while (str)
+	{
+		printf("main = %s", str);
+		free(str);
+		str = get_next_line(fd);
+	}
+	return (0);
+}
+
+test :
+salut\n
+je suis un poulet\n
+
+malloc buf
+buf read > salut
+line > salut
+2eme
+buf read > \nje s
+line > salut\nje s
+next > je s
+*/
---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 19:45:14 by nsion             #+#    #+#             */
+/*   Updated: 2023/03/04 18:21:44 by nsion            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
